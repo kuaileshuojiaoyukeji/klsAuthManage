c11f27ea8d19974a3d44f20124c1389149049f3b
xuhaojieheben <xuhaojieheben@126.com>
2019-03-18 10:42:51 +0800

    dev版本添加注释

--- KLS.AuthManage.Component.Tools/Core/GlobalFilters/ApiExpLogFilter.cs
+++ KLS.AuthManage.Component.Tools/Core/GlobalFilters/ApiExpLogFilter.cs
@@ -25,8 +25,9 @@ namespace KLS.AuthManage.Component.Tools.Core.GlobalFilters
             if (ex.Name == "DbUpdateConcurrencyException")
             {
                 var e = filterContext.Exception as DbUpdateConcurrencyException;
-                //测试版本并发处理
+                //测试版本并发处理  
                 //e.Entries.Single().Reload();
+                //测试地址版本测试
                 var entry = e.Entries.Single();
                 var databaseEntry = entry.GetDatabaseValues();
                 if (databaseEntry == null)
