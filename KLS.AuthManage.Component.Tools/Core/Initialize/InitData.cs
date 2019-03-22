b0601ca1a05d34790da33b4a4915e0cae047862d
xuhaojieheben <xuhaojieheben@126.com>
2019-03-22 11:11:43 +0800

    初始化数据设置

--- KLS.AuthManage.Component.Tools/Core/Initialize/InitData.cs
+++ KLS.AuthManage.Component.Tools/Core/Initialize/InitData.cs
@@ -1,19 +1,21 @@
 ﻿using KLS.AuthManage.Component.Data.Context;
 using KLS.AuthManage.Data.Model.Member;
+using KLS.AuthManage.IService.IUserService;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
-namespace KLS.AuthManage.Component.Data.Initialize
+namespace KLS.AuthManage.Component.Tools.Core.Initialize
 {
     public class InitData : DropCreateDatabaseIfModelChanges<EFDbContext>
     {
         protected override void Seed(EFDbContext context)
         {
-            User users = new User
+            User user = new User
             {
                 UserName = "admin",
                 NickName = "管理员",
@@ -22,30 +24,39 @@ namespace KLS.AuthManage.Component.Data.Initialize
                 Address = "",
                 Enabled = true,
                 //初始密码123456 用于数据库初始化系统部署时使用 请使用人员后期进行密码修改
-                PasswordHash = "U2FsdGVkX1/h+7bZlH4SS+f8ZEQs5R5zERTlZ5T+PKc=",
+                PasswordHash = "123456",
                 CreateTime = DateTime.Now,
                 UpdateDate = DateTime.Now,
             };
             //默认添加一个管理员角色
-            Role role = new Role
+            RoleModel role = new RoleModel
             {
-                Name = "admin",
+                RoleName = "admin",
                 OrderSort = 1,
-                Description = "超级管理员权限"
+                Description = "超级管理员权限",
+                Enabled = true
             };
+            var _authService = DependencyResolver.Current.GetService<IAuthService>();
+           
             //默认设置admin为admin角色 让其进行权限设置
-            context.Users.Add(users);
-            context.Roles.Add(role);
-            context.SaveChanges();
+           var _roleService = DependencyResolver.Current.GetService<IRoleService>();
+            _roleService.InsertAsync(role);
+
+            user.Roles.Add(new ApplicationUserRole
+            {
+                RoleId = 1,
+                UserId = 1
+            });
+            _authService.Register(user);
             //创建系统需要的存储过程
             context.Database.ExecuteSqlCommand(@"CREATE PROCEDURE [dbo].[select_testNames]
-    @id int,
-    @count int output
-AS
-SELECT Sys_Test.Id, Sys_Test.TestName from Sys_Test where
-     Sys_Test.Id > @id
+                @id int,
+                @count int output
+            AS
+            SELECT Sys_Test.Id, Sys_Test.TestName from Sys_Test where
+                 Sys_Test.Id > @id
 
-    set @count = (select COUNT(*) from Sys_Test where Sys_Test.Id > @id)");
+                set @count = (select COUNT(*) from Sys_Test where Sys_Test.Id > @id)");
             base.Seed(context);
         }
     }
