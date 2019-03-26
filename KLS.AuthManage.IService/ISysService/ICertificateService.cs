95095e911daf2a2b7a6d3e981244c1b9dc4ce636
xuhaojieheben <xuhaojieheben@126.com>
2019-03-26 15:24:31 +0800

    科目课程及章节接口添加

---
+++ KLS.AuthManage.IService/ISysService/ICertificateService.cs
@@ -0,0 +1,16 @@
+﻿using KLS.AuthManage.Component.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KLS.AuthManage.IService.ISysService
+{
+    public interface ICertificateService
+    {
+        List<Certificate> SelectAllCertificates();
+
+        Certificate SelectCertificateById(string certificateId);
+    }
+}
---
+++ KLS.AuthManage.Service/SysService/CourseService.cs
@@ -0,0 +1,25 @@
+﻿using KLS.AuthManage.Component.Data.Context;
+using KLS.AuthManage.Component.Data.DbService;
+using KLS.AuthManage.IService.ISysService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KLS.AuthManage.Service.SysService
+{
+    public class CourseService : ICourseService
+    {
+        private readonly IDbServiceReposity _dbServiceReposity;
+        public CourseService(IDbServiceReposity dbServiceReposity)
+        {
+            _dbServiceReposity = dbServiceReposity;
+        }
+
+        public List<Course> GetCoursesByCertificateId(string certId)
+        {
+            return _dbServiceReposity.Where<Course>(d => d.CertId == certId).ToList();
+        }
+    }
+}
