2ebb6b46b04a4eb92422a106553aa27b138923c5
xuhaojieheben <xuhaojieheben@126.com>
2019-03-28 13:41:38 +0800

    新增hzwxdb数据源 新增章节视频url字段

---
+++ KLS.AuthManage.Service/HzwxService/VideoExamService.cs
@@ -0,0 +1,36 @@
+﻿using KLS.AuthManage.Component.Data.Context;
+using KLS.AuthManage.Component.Data.DbService;
+using KLS.AuthManage.IService.IHzwxService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KLS.AuthManage.Service.HzwxService
+{
+    public class VideoExamService : IVideoExamService
+    {
+        private readonly IDbServiceReposity _dbServiceReposity;
+
+        public VideoExamService(IDbServiceReposity dbServiceReposity)
+        {
+            _dbServiceReposity = dbServiceReposity;
+        }
+
+        public List<VideoExam> GetAllVideoExams()
+        {
+            return _dbServiceReposity.All<VideoExam>().ToList();
+        }
+
+        public List<VideoExam> GetVideoExamByChapSecId(string chapSecId)
+        {
+            return _dbServiceReposity.Where<VideoExam>(d => d.ChapSecId == chapSecId).ToList();
+        }
+
+        public List<VideoExam> GetVideoExamByCourseId(string courseId)
+        {
+            return _dbServiceReposity.Where<VideoExam>(d => d.CourseID == courseId).ToList();
+        }
+    }
+}
